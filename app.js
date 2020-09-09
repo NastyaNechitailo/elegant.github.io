1b3bb461929d81f1e7b6ddedd2f864b1dd033770
Nastya <nastyarivz23@gmail.com>
2020-09-09 10:11:17 +0300

    fixed mistakes

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ navMenu.addEventListener("click", function() {
     });
 })
 
+// Tabs
+
+let tab = function() {
+    let tabNav = document.querySelectorAll(".tabs-nav-item");
+    let tabContent = document.querySelectorAll(".tab-nav-content");
+    let tabName;
+
+    tabNav.forEach( item=> {
+        item.addEventListener("click", selectTabNav)
+    })
+    
+    function selectTabNav() {
+        tabNav.forEach( item=> {
+            item.classList.remove("is-active");
+        })
+        this.classList.add("is-active");
+        tabName = this.getAttribute("data-tab-name");
+        selectTabContent(tabName)
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach( item=> {
+            if(item.classList.contains(tabName)) {
+                item.classList.add("is-active");
+            } else {
+                item.classList.remove("is-active");
+            }
+        })
+    }
+}
+tab()
